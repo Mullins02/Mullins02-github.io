1b58f10ce45178ed4930639444f0c97974212b41
Camron Mullins <113359510+Mullins02@users.noreply.github.com>
2024-07-12 21:34:33 -0230

    Replaced readme with index

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const name = "Your Name";
+    document.getElementById('name').textContent = name;
+    document.getElementById('footer-name').textContent = name;
+});
